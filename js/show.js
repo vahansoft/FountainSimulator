3550d20fe96559c8e874388aa26d02da8c9573f3
vahansoft <vahansoft@gmail.com>
2015-02-09 19:38:33 +0400

    First Commit

---
+++ js/show.js
@@ -0,0 +1,12 @@
+var sim = new Simulator();
+$(function () {
+	sim.init('canvas');
+	//  sim.playAudio('sound.mp3');
+	//  sim.light(12);
+	//  sim.wait(0.5);
+	//  sim.Helpers;
+	//  sim.Color;
+	/*START CODE HERE */
+	// sim.light(0)
+	/*END CODE HERE */
+});
